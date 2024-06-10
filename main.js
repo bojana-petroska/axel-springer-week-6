a9f36fde89ab35974e3b015411fa666ba32c5f7e
bojana-petroska <bojana.petroska@yahoo.com>
2024-06-10 15:35:01 +0200

    2st commit

--- main.js
+++ main.js
@@ -1,1 +1,63 @@
 console.log('bo');
+
+// Task 1: Arrays
+// Objective: Learn how to create and manipulate single and multidimensional arrays.
+// Instructions:
+// 1. Create a single-dimensional array containing the numbers 1 to 5.
+
+const array = [1, 2, 3, 4, 5];
+
+// b.. Create a two-dimensional array (matrix) representing the following data:
+
+const twoDimArray = [
+    [1, 2, 3],
+    [4, 5, 6], 
+    [7, 8, 9]
+];
+
+// 1 2 3
+// 4 5 6
+// 7 8 9
+
+// 2. Add the number 6 to the end of the single-dimensional array.
+
+//console.log(array.push(6));
+
+// 3. Access and print the element at the second row and third column of the matrix.
+
+//console.log(twoDimArray[1][2]);
+
+// Task 2: Stacks
+// Objective: Implement a basic stack using an array.
+// Hint: Use the built-in functions push and pop
+// Instructions:
+// 1. Create an empty array to represent the stack.
+
+const arr = [];
+
+// 2. Implement functions to push elements onto the stack and pop elements from the
+// stack.
+// 3. Push the elements 10, 20, and 30 onto the stack.
+
+arr.push(10, 20, 30);
+console.log(arr);
+
+// 4. Pop an element from the stack and print it.
+
+console.log(arr.pop());
+
+// Task 3: Queues
+// Objective: Implement a basic queue using an array.
+// Hint: You can use the built-in functions push and shift.
+// Instructions:
+// 1. Create an empty array to represent the queue.
+
+const newArray = [];
+
+// 2. Implement functions to enqueue (add) elements to the end of the queue and
+// dequeue (remove) elements from the front of the queue.
+
+
+
+// 3. Enqueue the elements 'a', 'b', and 'c' to the queue.
+// 4. Dequeue an element from the queue and print it.
