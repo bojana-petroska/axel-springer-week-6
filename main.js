bcbf0c415f4d107a4a095f2aeb74b16c5933c595
bojana-petroska <bojana.petroska@yahoo.com>
2024-06-10 17:49:36 +0200

    2st commit

--- main.js
+++ main.js
@@ -40,11 +40,11 @@ const arr = [];
 // 3. Push the elements 10, 20, and 30 onto the stack.
 
 arr.push(10, 20, 30);
-console.log(arr);
+//console.log(arr);
 
 // 4. Pop an element from the stack and print it.
 
-console.log(arr.pop());
+//console.log(arr.pop());
 
 // Task 3: Queues
 // Objective: Implement a basic queue using an array.
@@ -57,7 +57,61 @@ const newArray = [];
 // 2. Implement functions to enqueue (add) elements to the end of the queue and
 // dequeue (remove) elements from the front of the queue.
 
-
+const addElements = function(...elem) {
+    newArray.push(...elem);
+};
 
 // 3. Enqueue the elements 'a', 'b', and 'c' to the queue.
+addElements('a', 'b', 'c')
+console.log(newArray);
+
 // 4. Dequeue an element from the queue and print it.
+const removeElementsFromStart = function(elem) {
+    let firstElem = newArray.shift(0);
+    return firstElem;
+}
+
+console.log(removeElementsFromStart());
+
+// Task 4: Dictionaries (Objects)
+// Objective: Learn how to create and manipulate dictionaries (objects).
+// Hint: You can do this by:
+// Instructions:
+// 1. Create an object to represent a dictionary with keys 'name', 'age', and 'city'.
+
+let dictionary = {
+    firstName: 'Bo',
+    age: 37,
+    city: 'Berlin'
+};
+
+// 2. Assign appropriate values to these keys.
+// 3. Add a new key 'country' with a value to the object.
+
+dictionary.country = 'Deutschland';
+
+// 4. Print the value of the 'name' key.
+
+console.log(dictionary.firstName);
+console.log(dictionary.country);
+console.log(dictionary);
+
+// Task 5: Sets
+// Objective: Learn how to create and manipulate sets.
+// Instructions:
+// 1. Create a set and add the numbers 1, 2, 3, 4, and 5 to it.
+
+const numbers = new Set([1, 2, 3, 4, 5]);
+numbers.add(4);
+numbers.add(6);
+console.log(numbers);
+
+// 2. Add a duplicate number to the set and observe what happens.
+// 3. Remove a number from the set.
+
+numbers.delete(2);
+console.log(numbers);
+
+// 4. Print all the elements of the set.
+
+
